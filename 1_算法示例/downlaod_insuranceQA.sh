44c3385858c5b46eb5640793fd3df841205d2921
sn-911 <512428493@qq.com>
2022-07-09 23:44:13 +0800

    add

---
+++ 1_算法示例/downlaod_insuranceQA.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget -L https://github.com/shuzi/insuranceQA/archive/refs/heads/master.zip
+unzip master.zip
+mkdir datas
+mv insuranceQA-master/V2 datas/
+rm -rf insuranceQA-master/
+rm -f master.zip
+cp vocabulary datas/
---
+++ 1_算法示例/download_GoogleNews-vectors-negative300.bin.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -c "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz"
+gzip -d GoogleNews-vectors-negative300.bin.gz
