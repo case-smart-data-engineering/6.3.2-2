ec88cd097cc74a53deeb93f17bd227a35c4efe1a
sn-911 <512428493@qq.com>
2022-07-10 19:20:04 +0800

    add

--- 1_算法示例/downlaod_insuranceQA.sh
+++ 1_算法示例/downlaod_insuranceQA.sh
@@ -4,6 +4,10 @@ wget -L https://github.com/shuzi/insuranceQA/archive/refs/heads/master.zip
 unzip master.zip
 mkdir datas
 mv insuranceQA-master/V2 datas/
+cd ./datas/V2
+gunzip *.gz
+cd ..
+cd ..
 rm -rf insuranceQA-master/
 rm -f master.zip
 cp ./datas/V2/vocabulary ./datas/
