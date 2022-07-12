31dd05006f5e96451a99b059d851aa2fabc3e82b
sn-911 <512428493@qq.com>
2022-07-12 11:09:12 +0800

    add

--- 1_算法示例/download_GoogleNews-vectors-negative300.bin.sh
+++ 1_算法示例/download_GoogleNews-vectors-negative300.bin.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-wget -c "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz"
-gzip -d GoogleNews-vectors-negative300.bin.gz
+#wget -c "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz"
+#gzip -d GoogleNews-vectors-negative300.bin.gz
+
+wget -c "https://s3-eu-west-1.amazonaws.com/pfigshare-u-files/13774556/GoogleNewsvectorsnegative300.bin"
+mv GoogleNewsvectorsnegative300.bin GoogleNews-vectors-negative300.bin
